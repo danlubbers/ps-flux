f371cf35514b21e0c5e9adddd68371aaa1ca5ed0
Dan Lubbers <lubbers.dan@gmail.com>
2020-04-19 18:27:24 -0400

    Finished module props, state, lifecycle methods and keys

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -18,9 +18,29 @@ class Courses extends Component {
 
   render() {
     return (
-      <h2>
-        Courses
-      </h2>
+      <>
+        <h2>Courses</h2>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Author's ID</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.courses.map(e => {
+              return (
+                <tr key={e.id}>
+                  <td>{e.title}</td>
+                  <td>{e.authorId}</td>
+                  <td>{e.category}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </>
     )
   }
 }
